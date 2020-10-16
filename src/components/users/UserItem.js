d4a18598280bf133d7fd86e0c2e01780b4b41d1b
Shaker Kattoula <smkattoula@gmail.com>
2020-10-16 14:18:08 -0700

    refactor stateless class components into functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,31 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user }) => {
+  const { login, avatar_url, html_url } = user;
 
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={html_url}
-            className="btn btn-dark btn-sm my-1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            View More
-          </a>
-        </div>
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          className="btn btn-dark btn-sm my-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
